d7c92b2c99d6ad15b22fd1ab3cf83c981cfc4042
SectorPriz <mil-tank@bk.ru>
2019-11-03 19:42:42 +0400

    ДЗ JS

--- js/script.js
+++ js/script.js
@@ -5,16 +5,18 @@ var arrival = popup.querySelector("[name=date-arrival]");
 var departure = popup.querySelector("[name=date-departure]");
 var grown = popup.querySelector("[name=grown]");
 var children = popup.querySelector("[name=children]");
-button.addEventListener("click", function (evt) {
+button.addEventListener("click", function(evt) {
   arrival.focus();
   if (popup.classList.contains("search-form-box-show")) {
     evt.preventDefault();
-    popup.classList.toggle("search-form-box-hidden");}
+    popup.classList.toggle("search-form-box-hidden");
+  }
 });
-form.addEventListener("submit", function (evt) {
+form.addEventListener("submit", function(evt) {
   if (!arrival.value || !departure.value || !grown.value || !children.value) {
     evt.preventDefault();
     popup.classList.remove("search-form-box-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("search-form-box-error");}
+    popup.classList.add("search-form-box-error");
+  }
 });
