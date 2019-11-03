7ef3e96652996c33bf2d7f2de84d2d15894710cd
SectorPriz <mil-tank@bk.ru>
2019-11-03 17:51:56 +0400

    работа с js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var button = document.querySelector(".hotels-search-button");
+var popup = document.querySelector(".search-form-box-show");
+var form = document.querySelector(".search-form");
+var arrival = popup.querySelector("[name=date-arrival]");
+var departure = popup.querySelector("[name=date-departure]");
+var grown = popup.querySelector("[name=grown]");
+var children = popup.querySelector("[name=children]");
+button.addEventListener("click", function (evt) {
+  arrival.focus();
+  if (popup.classList.contains("search-form-box-show")) {
+    evt.preventDefault();
+    popup.classList.toggle("search-form-box-hidden");}
+});
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !grown.value || !children.value) {
+    evt.preventDefault();
+    popup.classList.add("search-form-box-error");}
+});
